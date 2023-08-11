c43838e24dd88a312b92c4a274f0a57884e13610
BalaSubrahmanyam1022 <109712535+BalaSubrahmanyam1022@users.noreply.github.com>
2023-08-11 15:53:33 +0530

    Add files via upload

---
+++ balu/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+  int a;
+  int b;
+  int result;
+  cout<<"enter first num"  ;
+  cin>>a;
+  cout<<"enter second num";
+  cin>>b;
+  result=a+b;
+  cout<<result;
+  return 0;
+}
---
+++ balu/cpp programs/first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  int b;
+  int result;
+  cout<<"enter first num"  ;
+  cin>>a;
+  cout<<"enter second num";
+  cin>>b;
+  result=a+b;
+  cout<<result;
+  return 0;
+}
